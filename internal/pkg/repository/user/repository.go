722bdd6de718d70b1a7b6e024d2d36469cba9cef
rinnothing <58170199+rinnothing@users.noreply.github.com>
2024-12-14 20:08:24 +0300

    feat: implement user repository

--- internal/pkg/repository/user/repository.go
+++ internal/pkg/repository/user/repository.go
@@ -1,5 +1,53 @@
 package user
 
-import "errors"
+import (
+	"context"
+	"sync"
+)
 
-var ErrNotFound = errors.New("not found")
+type Repo struct {
+	mx    *sync.RWMutex
+	users map[string]int
+	cnt   int
+}
+
+func NewRepo() *Repo {
+	return &Repo{
+		mx:    new(sync.RWMutex),
+		users: make(map[string]int),
+	}
+}
+
+func (r *Repo) GetID(ctx context.Context, username string) (int, error) {
+	r.mx.RLock()
+	var once sync.Once
+	defer once.Do(r.mx.RUnlock)
+
+	if err := ctx.Err(); err != nil {
+		return 0, err
+	}
+
+	val, ok := r.users[username]
+	if ok {
+		return val, nil
+	}
+
+	r.mx.RUnlock()
+
+	r.mx.Lock()
+	defer r.mx.Unlock()
+
+	if err := ctx.Err(); err != nil {
+		return 0, err
+	}
+
+	val, ok = r.users[username]
+	if ok {
+		return val, nil
+	}
+
+	r.users[username] = r.cnt
+	r.cnt++
+
+	return r.cnt - 1, nil
+}
