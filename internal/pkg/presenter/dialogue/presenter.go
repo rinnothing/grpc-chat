1a4ff10acb5af9b61758aae0ae2e27a1017c4d02
rinnothing <58170199+rinnothing@users.noreply.github.com>
2024-12-14 20:30:37 +0300

    feat: implement dialogue temporary mock

--- internal/pkg/presenter/dialogue/presenter.go
+++ internal/pkg/presenter/dialogue/presenter.go
@@ -1,5 +1,53 @@
 package dialogue
 
-import "errors"
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+
+	"github.com/rinnothing/grpc-chat/internal/pkg/model"
+)
 
 var ErrNotAllowed = errors.New("user not allowed")
+
+type Presenter struct {
+	input  io.Reader
+	output io.Writer
+}
+
+func NewDialoguePresenter(input io.Reader, output io.Writer) *Presenter {
+	return &Presenter{
+		input:  input,
+		output: output,
+	}
+}
+
+func (p *Presenter) AskAccept(_ context.Context, user *model.User, message *model.Message) error {
+	_, err := fmt.Fprintf(p.output,
+		"User %s wants to chat with you:\n"+
+			"%s\n"+
+			"allow | deny\n",
+		user.Username, message.Text,
+	)
+	if err != nil {
+		return err
+	}
+
+	var answer string
+	for {
+		_, err = fmt.Fscanln(p.input, &answer)
+		if err != nil {
+			return err
+		}
+
+		switch answer {
+		case "allow":
+			return nil
+		case "deny":
+			return ErrNotAllowed
+		default:
+			_, err = fmt.Fprintf(p.output, "%s is not a correct option, choose allow or deny\n", answer)
+		}
+	}
+}
