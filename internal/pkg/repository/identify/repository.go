5398b44b14735258d73a9dfeca560b3016147cb4
rinnothing <58170199+rinnothing@users.noreply.github.com>
2024-12-14 19:04:02 +0300

    feat: implement identify

---
+++ internal/pkg/repository/identify/repository.go
@@ -0,0 +1,25 @@
+package identify
+
+import (
+	"context"
+	"net"
+
+	"github.com/rinnothing/grpc-chat/internal/pkg/model"
+)
+
+type Repo struct {
+	user model.User
+}
+
+func NewIdentifyRepository(IPv4 net.IP, username string) *Repo {
+	return &Repo{
+		user: model.User{
+			Username: username,
+			IPv4:     IPv4,
+		},
+	}
+}
+
+func (r *Repo) Myself(_ context.Context) *model.User {
+	return &r.user
+}
