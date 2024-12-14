9a545be081c9e4a35284d27939f583efd5c5a5d4
rinnothing <58170199+rinnothing@users.noreply.github.com>
2024-12-14 19:31:52 +0300

    feat: implement connections repository

--- internal/pkg/repository/connections/repository.go
+++ internal/pkg/repository/connections/repository.go
@@ -1,8 +1,115 @@
 package connections
 
-import "errors"
+import (
+	"bytes"
+	"context"
+	"errors"
+	"sync"
+
+	"github.com/rinnothing/grpc-chat/internal/pkg/model"
+)
 
 var (
 	ErrAlreadyConnected = errors.New("already connected")
-	ErrNotConnected = errors.New("not connected")
+	ErrNotConnected     = errors.New("not connected")
 )
+
+type Repo struct {
+	mx   *sync.RWMutex
+	data map[int]*model.User
+}
+
+func NewIdentifyRepo() *Repo {
+	return &Repo{
+		mx:   new(sync.RWMutex),
+		data: make(map[int]*model.User),
+	}
+}
+
+func (r *Repo) Connect(ctx context.Context, user *model.User) error {
+	err := func() error {
+		r.mx.RLock()
+		defer r.mx.RUnlock()
+
+		if ctx.Err() != nil {
+			return ctx.Err()
+		}
+
+		val, ok := r.data[user.ID]
+		if ok && !bytes.Equal(val.IPv4, user.IPv4) {
+			return ErrAlreadyConnected
+		}
+
+		return nil
+	}()
+	if err != nil {
+		return err
+	}
+
+	r.mx.Lock()
+	defer r.mx.Unlock()
+
+	if ctx.Err() != nil {
+		return ctx.Err()
+	}
+
+	val, ok := r.data[user.ID]
+	if ok && !bytes.Equal(val.IPv4, user.IPv4) {
+		return ErrAlreadyConnected
+	}
+
+	r.data[user.ID] = user
+
+	return nil
+}
+
+func (r *Repo) IsConnected(ctx context.Context, user *model.User) error {
+	r.mx.RLock()
+	defer r.mx.RUnlock()
+
+	if err := ctx.Err(); err != nil {
+		return err
+	}
+
+	_, ok := r.data[user.ID]
+	if !ok {
+		return ErrNotConnected
+	}
+	return nil
+}
+
+func (r *Repo) Disconnect(ctx context.Context, user *model.User) error {
+	err := func() error {
+		r.mx.RLock()
+		defer r.mx.RUnlock()
+
+		if ctx.Err() != nil {
+			return ctx.Err()
+		}
+
+		_, ok := r.data[user.ID]
+		if !ok {
+			return ErrNotConnected
+		}
+
+		return nil
+	}()
+	if err != nil {
+		return err
+	}
+
+	r.mx.Lock()
+	defer r.mx.Unlock()
+
+	if ctx.Err() != nil {
+		return ctx.Err()
+	}
+
+	_, ok := r.data[user.ID]
+	if !ok {
+		return ErrNotConnected
+	}
+
+	delete(r.data, user.ID)
+	return nil
+}
