cb26247c3f1e702b8dfe0f90eee46d6772d90af9
rinnothing <58170199+rinnothing@users.noreply.github.com>
2024-12-14 19:52:15 +0300

    feat: implement message repository

---
+++ internal/pkg/repository/message/repository.go
@@ -0,0 +1,43 @@
+package message
+
+import (
+	"context"
+	"sync"
+	"time"
+
+	"github.com/rinnothing/grpc-chat/internal/pkg/model"
+)
+
+// Repo will be in memory repo for now
+// obviously I will replace it with database later
+type Repo struct {
+	mx       *sync.RWMutex
+	messages []Entry
+}
+
+type Entry struct {
+	userID  int
+	message model.Message
+}
+
+func NewMessageRepo() *Repo {
+	return &Repo{
+		mx:       new(sync.RWMutex),
+		messages: make([]Entry, 0),
+	}
+}
+
+func (r *Repo) PutID(ctx context.Context, user *model.User, text string, time time.Time) (int, error) {
+	r.mx.Lock()
+	defer r.mx.Unlock()
+
+	id := len(r.messages)
+	r.messages = append(r.messages, Entry{user.ID, model.Message{
+		ID:   id,
+		User: user,
+		Text: text,
+		Time: time,
+	}})
+
+	return id, nil
+}
